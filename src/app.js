34c49bdad7d88b3f86d489a1c2979c97cce42862
Rajesh Bagguva <rajesh.bagguva@tcs.com>
2024-09-18 22:39:18 +0530

    minor change

--- src/app.js
+++ src/app.js
@@ -6,6 +6,10 @@ app.use("/test", (req, res) => {
   res.end("This is for /test");
 });
 
+app.use("/hello", (req, res) => {
+  res.end("This is for /hello");
+});
+
 app.listen("7777", () => {
   console.log("Server is running on 7777");
 });
