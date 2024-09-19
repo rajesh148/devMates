08583b47d3be5ce4c7f2e08021e3ee3a836b4f40
Rajesh Bagguva <rajesh.bagguva@tcs.com>
2024-09-19 06:29:51 +0530

    Playing with GET_POST_DELETE

--- src/app.js
+++ src/app.js
@@ -2,12 +2,27 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test", (req, res) => {
-  res.end("This is for /test");
+// app.use("/user", (req, res) => {
+//   res.send("hahahahahaha");
+// });
+
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Rajesh", lastName: "Bagguva" });
 });
 
-app.use("/hello", (req, res) => {
-  res.end("This is for /hello");
+app.post("/user", async (req, res) => {
+  console.log(req.body);
+  //Save the data into DB first
+  res.send("User data POST successfully");
+});
+
+app.delete("/user", (req, res) => {
+  //Delete the data into DB first then do the send message
+  res.send("User data delete successfully");
+});
+
+app.use("/test", (req, res) => {
+  res.end("This is for /test");
 });
 
 app.listen("7777", () => {
