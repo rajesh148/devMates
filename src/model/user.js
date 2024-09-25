c09eecd45e3c6ea8985a6cb08985ae18bf12671a
Rajesh Bagguva <rajeshbagguvaiiitn@gmail.com>
2024-09-25 19:25:32 +0530

    validatio

--- src/model/user.js
+++ src/model/user.js
@@ -1,25 +1,54 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, "First Name is required"],
+      minLength: [4, "First Name Length should be more than 3"],
+      maxLength: [50, "First Name Length should be less than 50"],
+    },
+    lastName: {
+      type: String,
+      required: [true, "Last Name is required"],
+      minLength: [4, "Last Name Length should be more than 3"],
+      maxLength: [50, "Last Name Length should be less than 50"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minLength: [4, "Password Length should be more than 3"],
+      maxLength: [50, "Password Length should be less than 50"],
+    },
+    age: {
+      type: Number,
+      min: [18, "You must be at least 18 years old!"], // Optional with validation
+    },
+    gender: {
+      type: String,
+    },
+    phone: {
+      type: String,
+      required: [true, 'User phone number required'],
+      validate: {
+        validator: function(v) {
+          // Custom validation: Phone number should start with 6-9 and be 10 digits long
+          return /^[6-9]\d{9}$/.test(v);
+        },
+        message: props => `${props.value} is not a valid phone number! It must be a 10-digit number starting with 6-9.`
+      }
+    }
   },
-  lastName: {
-    type: String,
-  },
-  email: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const User = mongoose.model("User", userSchema);
 
